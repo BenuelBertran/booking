b910983cf7f9308d4de699eddda5d244adb9bcd3
Lex_TM <BenuelBertran@gmail.com>
2018-11-10 19:40:52 +0300

    Начал вести историю проекта Booking

---
+++ js/map.js
@@ -0,0 +1,7 @@
+/* jshint node: true */
+/* jshint browser: true */
+/* global window */
+"use strict";
+
+//Временно включаем активный режим окна
+document.querySelector(".map").classList.remove("map--faded");
