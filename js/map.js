209e1dc7f8d425f4f7687a097eea72ce10c09fb7
Lex_TM <BenuelBertran@gmail.com>
2018-11-14 18:51:43 +0300

    Обновление функционала, мелкие исправления.
    - Настроил Активное и Неактивное состояние страницы;
    - Реализовал показ похожих объявлений при нажатии на маркеры;
    
    TODO:
    - Синхронизация полей: комнаты, цена и гости;
    - Настройка валидации формы при отправке;

--- js/map.js
+++ js/map.js
@@ -3,54 +3,47 @@
 /* global window */
 "use strict";
 
-///// Страница в неактивном состоянии /////
+
+///// Переменные /////
+
 
 //Список объявлений
 var lodgingOffersList = document.querySelector(".map");
+//Список маркеров объявлений
+var pinsList = document.querySelector(".map__pins");
 //Главный маркер
 var mainPin = lodgingOffersList.querySelector(".map__pin--main");
+//Шаблон маркера объявления
+var pinTemplate = document.querySelector("template").content.querySelector(".map__pin");
 //Форма объявления
 var lodgingOfferForm = document.querySelector(".notice__form");
 //Шапка формы объявлений
 var formHeader = lodgingOfferForm.querySelector(".notice__header");
 //Блоки формы объявлений
 var formBlocks = lodgingOfferForm.querySelectorAll(".form__element");
+//Поле Адрес формы объявления
+var addressField = document.getElementById("address");
+//Блок с фильтром объявлений
+var lodgingOfferFilter = document.querySelector(".map__filters-container");
+//Шаблон объявления
+var offerTemplate = document.querySelector("template").content.querySelector(".map__card");
+//Независимая копия шаблона объявления
+var offerElement = offerTemplate.cloneNode(true);
 
-//Блокировка формы по умолчанию
-formHeader.disabled = true;
-for (var i = 0; i < formBlocks.length; i++) {
-  formBlocks[i].disabled = true;
-}
 
-//Разблокировка формы
-var onPinMoved = function() {
-  lodgingOffersList.classList.remove("map--faded");
-  lodgingOfferForm.classList.remove("notice__form--disabled");
-  formHeader.disabled = false;
-  for (var i = 0; i < formBlocks.length; i++) {
-    formBlocks[i].disabled = false;
-  }
-};
-
-mainPin.addEventListener("mouseup", onPinMoved);
-
-///// Страница в активном состоянии /////
-
-//document.querySelector(".map").classList.remove("map--faded");
+///// Функции /////
 
 
 //Случайное число в диапазоне
 var getRandom = function (min, max) {
   return Math.random() * (max - min) + min;
 };
-
 //Случайная длина массива в диапазоне
 var getRandomLength = function (array, min, max) {
   array.length = Math.round(getRandom(min, max));
 
   return array;
 };
-
 //Перемешивание элементов массива
 var shuffle = function (array) {
   for (var i = array.length - 1; i > 0; i--) {
@@ -61,13 +54,16 @@ var shuffle = function (array) {
   }
   return array;
 };
-
 //Создание независимой копии массива (без дочерних "объектов")
 var cloneArray = function(array) {
   var clone = array.slice(0);
 
   return clone;
 };
+
+
+///// Данные /////
+
 
 //Заголовки объявлений
 var offerTitles = [
@@ -119,6 +115,10 @@ var lodgingPhotos = [
   "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
   "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
 ];
+
+
+///// Шаблоны /////
+
 
 //Объявления
 var lodgingOffers = [
@@ -300,32 +300,6 @@ var lodgingOffers = [
   }
 ];
 
-//Список маркеров объявлений
-var pinsList = document.querySelector(".map__pins");
-
-//Шаблон маркера объявления
-var pinTemplate = document.querySelector("template").content.querySelector(".map__pin");
-
-////Отрисовка маркеров на странице
-//for (var i = 0; i < lodgingOffers.length; i++) {
-//  var pinElement = pinTemplate.cloneNode(true);
-//  pinElement.style.left = "" + lodgingOffers[i].location.x + "px";
-//  pinElement.style.top = "" + lodgingOffers[i].location.y + "px";
-//  pinElement.querySelector("img").src = lodgingOffers[i].author.avatar;
-//  pinElement.querySelector("img").alt = lodgingOffers[i].offer.title;
-//
-//  pinsList.appendChild(pinElement);
-//}
-
-//Блок с фильтром объявлений
-var lodgingOfferFilter = document.querySelector(".map__filters-container");
-
-//Шаблон объявления
-var offerTemplate = document.querySelector("template").content.querySelector(".map__card");
-
-//Независимая копия шаблона объявления
-var offerElement = offerTemplate.cloneNode(true);
-
 //Добавление недостающих классов в шаблон объявления
 offerElement.querySelector("h3").classList.add("popup__title");
 offerElement.querySelector("p:first-of-type").classList.add("popup__text--address");
@@ -337,60 +311,168 @@ offerElement.querySelector("p:last-of-type").classList.add("popup__description")
 offerElement.querySelector("ul:last-of-type").classList.add("popup__photos");
 
 //Заполнение шаблона объявления подготовленными данными
-offerElement.querySelector(".popup__title").textContent = lodgingOffers[0].offer.title;
-offerElement.querySelector(".popup__text--address").children[0].textContent = lodgingOffers[0].offer.address;
-offerElement.querySelector(".popup__text--price").textContent = lodgingOffers[0].offer.price + "₽/ночь";
-if (lodgingOffers[0].offer.type === "flat") {
-  offerElement.querySelector(".popup__type").textContent = "Квартира";
-  } else if (lodgingOffers[0].offer.type === "bungalo") {
-      offerElement.querySelector(".popup__type").textContent = "Бунгало";
-  } else if (lodgingOffers[0].offer.type === "house") {
-      offerElement.querySelector(".popup__type").textContent = "Дом";
-  } else if (lodgingOffers[0].offer.type === "palace") {
-      offerElement.querySelector(".popup__type").textContent = "Дворец";
+var makeLodgingOffer = function (index) {
+  offerElement.querySelector(".popup__title").textContent = lodgingOffers[index].offer.title;
+  offerElement.querySelector(".popup__text--address").children[0].textContent = lodgingOffers[index].offer.address;
+  offerElement.querySelector(".popup__text--price").textContent = lodgingOffers[index].offer.price + "₽/ночь";
+  if (lodgingOffers[index].offer.type === "flat") {
+    offerElement.querySelector(".popup__type").textContent = "Квартира";
+    } else if (lodgingOffers[index].offer.type === "bungalo") {
+        offerElement.querySelector(".popup__type").textContent = "Бунгало";
+    } else if (lodgingOffers[index].offer.type === "house") {
+        offerElement.querySelector(".popup__type").textContent = "Дом";
+    } else if (lodgingOffers[index].offer.type === "palace") {
+        offerElement.querySelector(".popup__type").textContent = "Дворец";
+    }
+  offerElement.querySelector(".popup__text--capacity").textContent = lodgingOffers[index].offer.rooms + " комнаты для " + lodgingOffers[index].offer.guests + " гостей";
+  offerElement.querySelector(".popup__text--time").textContent = "Заезд после " + lodgingOffers[index].offer.checkin + ", выезд до " + lodgingOffers[index].offer.checkout;
+
+  if (lodgingOffers[index].offer.features.length === 5) {
+    offerElement.querySelector(".feature--conditioner").style.display = "none";
+  } else if (lodgingOffers[index].offer.features.length === 4) {
+    offerElement.querySelector(".feature--conditioner").style.display = "none";
+    offerElement.querySelector(".feature--elevator").style.display = "none";
+  } else if (lodgingOffers[index].offer.features.length === 3) {
+    offerElement.querySelector(".feature--conditioner").style.display = "none";
+    offerElement.querySelector(".feature--elevator").style.display = "none";
+    offerElement.querySelector(".feature--washer").style.display = "none";
+  } else if (lodgingOffers[index].offer.features.length === 2) {
+    offerElement.querySelector(".feature--conditioner").style.display = "none";
+    offerElement.querySelector(".feature--elevator").style.display = "none";
+    offerElement.querySelector(".feature--washer").style.display = "none";
+    offerElement.querySelector(".feature--parking").style.display = "none";
+  } else if (lodgingOffers[index].offer.features.length === 1) {
+    offerElement.querySelector(".feature--conditioner").style.display = "none";
+    offerElement.querySelector(".feature--elevator").style.display = "none";
+    offerElement.querySelector(".feature--washer").style.display = "none";
+    offerElement.querySelector(".feature--parking").style.display = "none";
+    offerElement.querySelector(".feature--dishwasher").style.display = "none";
+  } else if (lodgingOffers[index].offer.features.length === 0) {
+    offerElement.querySelector(".feature--conditioner").style.display = "none";
+    offerElement.querySelector(".feature--elevator").style.display = "none";
+    offerElement.querySelector(".feature--washer").style.display = "none";
+    offerElement.querySelector(".feature--parking").style.display = "none";
+    offerElement.querySelector(".feature--dishwasher").style.display = "none";
+    offerElement.querySelector(".feature--wifi").style.display = "none";
   }
-offerElement.querySelector(".popup__text--capacity").textContent = lodgingOffers[0].offer.rooms + " комнаты для " + lodgingOffers[0].offer.guests + " гостей";
-offerElement.querySelector(".popup__text--time").textContent = "Заезд после " + lodgingOffers[0].offer.checkin + ", выезд до " + lodgingOffers[0].offer.checkout;
+  offerElement.querySelector(".popup__description").textContent = lodgingOffers[index].offer.description;
+  for (var i = 0; i < lodgingOffers[index].offer.photos.length; i++) {
+    var photosElement = offerElement.querySelector(".popup__photos").children[0].cloneNode(true);
+    photosElement.querySelector("img").src = lodgingOffers[index].offer.photos[i];
 
-if (lodgingOffers[0].offer.features.length === 5) {
-  offerElement.querySelector(".feature--conditioner").style.display = "none";
-} else if (lodgingOffers[0].offer.features.length === 4) {
-  offerElement.querySelector(".feature--conditioner").style.display = "none";
-  offerElement.querySelector(".feature--elevator").style.display = "none";
-} else if (lodgingOffers[0].offer.features.length === 3) {
-  offerElement.querySelector(".feature--conditioner").style.display = "none";
-  offerElement.querySelector(".feature--elevator").style.display = "none";
-  offerElement.querySelector(".feature--washer").style.display = "none";
-} else if (lodgingOffers[0].offer.features.length === 2) {
-  offerElement.querySelector(".feature--conditioner").style.display = "none";
-  offerElement.querySelector(".feature--elevator").style.display = "none";
-  offerElement.querySelector(".feature--washer").style.display = "none";
-  offerElement.querySelector(".feature--parking").style.display = "none";
-} else if (lodgingOffers[0].offer.features.length === 1) {
-  offerElement.querySelector(".feature--conditioner").style.display = "none";
-  offerElement.querySelector(".feature--elevator").style.display = "none";
-  offerElement.querySelector(".feature--washer").style.display = "none";
-  offerElement.querySelector(".feature--parking").style.display = "none";
-  offerElement.querySelector(".feature--dishwasher").style.display = "none";
-} else if (lodgingOffers[0].offer.features.length === 0) {
-  offerElement.querySelector(".feature--conditioner").style.display = "none";
-  offerElement.querySelector(".feature--elevator").style.display = "none";
-  offerElement.querySelector(".feature--washer").style.display = "none";
-  offerElement.querySelector(".feature--parking").style.display = "none";
-  offerElement.querySelector(".feature--dishwasher").style.display = "none";
-  offerElement.querySelector(".feature--wifi").style.display = "none";
+    offerElement.querySelector(".popup__photos").append(photosElement);
+  }
+  offerElement.querySelector(".popup__photos").removeChild(offerElement.querySelector(".popup__photos").children[0]);
+  offerElement.querySelector(".popup__avatar").src = lodgingOffers[index].author.avatar;
+
+  //Отображение объявления на странице
+    lodgingOffersList.insertBefore(offerElement, lodgingOfferFilter);
+};
+
+//Отрисовка маркеров на странице
+for (var j = 0; j < lodgingOffers.length; j++) {
+  var pinElement = pinTemplate.cloneNode(true);
+  pinElement.style.left = "" + lodgingOffers[j].location.x + "px";
+  pinElement.style.top = "" + lodgingOffers[j].location.y + "px";
+  pinElement.querySelector("img").src = lodgingOffers[j].author.avatar;
+  pinElement.querySelector("img").alt = lodgingOffers[j].offer.title;
+  pinElement.classList.add("hidden");
+  pinsList.appendChild(pinElement);
 }
-offerElement.querySelector(".popup__description").textContent = lodgingOffers[0].offer.description;
 
-for (var i = 0; i < lodgingOffers[0].offer.photos.length; i++) {
-  var photosElement = offerElement.querySelector(".popup__photos").children[0].cloneNode(true);
-  photosElement.querySelector("img").src = lodgingOffers[0].offer.photos[i];
 
-  offerElement.querySelector(".popup__photos").append(photosElement);
+///// Страница в неактивном состоянии /////
+
+
+//Состояние неактивной страницы
+formHeader.disabled = true;
+for (var i = 0; i < formBlocks.length; i++) {
+  formBlocks[i].disabled = true;
 }
-offerElement.querySelector(".popup__photos").removeChild(offerElement.querySelector(".popup__photos").children[0]);
-offerElement.querySelector(".popup__avatar").src = lodgingOffers[0].author.avatar;
+//Адрес главного маркера (без указателя)
+addressField.placeholder = "600" + ", 375";
 
 
-////Добавление готового шаблона объявления на страницу
-//lodgingOffersList.insertBefore(offerElement, lodgingOfferFilter);
+///// Страница в активном состоянии /////
+
+
+//Состояние активной страницы
+var onPinMoved = function() {
+  //Отключение невидимости полей
+  lodgingOffersList.classList.remove("map--faded");
+  lodgingOfferForm.classList.remove("notice__form--disabled");
+  formHeader.disabled = false;
+  for (var i = 0; i < formBlocks.length; i++) {
+    formBlocks[i].disabled = false;
+  }
+  for (var j = 0; j < pinsList.children.length; j++) {
+    if (pinsList.children[j].hasAttribute("style")) {
+      pinsList.children[j].classList.remove("hidden");
+    }
+  }
+  //Адрес главного маркера с указателем
+  addressField.placeholder = "600" + ", 428";
+};
+
+//Активация страницы
+mainPin.addEventListener("mouseup", onPinMoved);
+
+//Показ похожего объявления по клику
+pinsList.addEventListener("click", function(evt) {
+  var target = evt.target;
+  while (target != this) {
+    if (target.hasAttribute("style")) {
+      if (target === pinsList.children[2]) {
+        makeLodgingOffer(0);
+      } else if (target === pinsList.children[3]) {
+        makeLodgingOffer(1);
+      } else if (target === pinsList.children[4]) {
+        makeLodgingOffer(2);
+      } else if (target === pinsList.children[5]) {
+        makeLodgingOffer(3);
+      } else if (target === pinsList.children[6]) {
+        makeLodgingOffer(4);
+      } else if (target === pinsList.children[7]) {
+        makeLodgingOffer(5);
+      } else if (target === pinsList.children[8]) {
+        makeLodgingOffer(6);
+      } else if (target === pinsList.children[9]) {
+        makeLodgingOffer(7);
+      }
+    }
+    target = target.parentNode;
+  }
+});
+
+
+//===========================================
+
+var lodgingOfferTitle = document.getElementById("title");
+var lodgingOfferPrice = document.getElementById("price");
+var lodgingOfferTimein = document.getElementById("timein");
+var lodgingOfferTimeout = document.getElementById("timeout");
+var lodgingOfferType = document.getElementById("type");
+
+lodgingOfferForm.action = "https://js.dump.academy/keksobooking";
+
+lodgingOfferTitle.required = true;
+lodgingOfferTitle.setAttribute("minlength", "30");
+lodgingOfferTitle.setAttribute("maxlength", "100");
+
+lodgingOfferPrice.required = true;
+lodgingOfferPrice.max = 1000000;
+
+addressField.disabled = true;
+
+//var minPrice = [
+//  "0",
+//  "1000",
+//  "5000",
+//  "10000"
+//];
+//for (var i = 0; i < lodgingOfferType.children.length; i++) {
+//  if (lodgingOfferType.children[i].hasAttribute("selected")) {
+//    lodgingOfferType.min = minPrice[i];
+//    lodgingOfferType.placeholder = minPrice[i];
+//  }
+//}
